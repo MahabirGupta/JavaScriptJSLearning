8fde7ff1ef2afd243886991fc079372e8176f405
MahabirGupta <mahabirg81@gmail.com>
2023-08-30 14:04:53 +0800

    complete variable assignment

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,17 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-40 + 8 + 23 - 10;
+
 console.log(40 + 8 + 23 - 10);
+console.log("Mahabir");
+console.log(42);
+
+// declaring a variable
+let firstName = "Anish";
+console.log(firstName);
+
+let second_name = "Gupta";
+console.log(second_name);
+
+let PI = 3.145;
+
+let myFirstJob = "Teacher";
+let myCurrentJob = "Software Developer";
